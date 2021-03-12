1c42d2fad367993b0b5977d82164b6e6dbf232ea
Ariston Lorenzo <ariston.thomas@daum.net>
2021-03-11 22:29:11 -0500

    AK: Fully translated the folder from C to CPP
    Signed-off-by: Ariston Lorenzo <ariston.thomas@daum.net>

---
+++ ak/StringBuilder.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "string.h"
+#include "vector.h"
+
+namespace AK {
+
+class StringBuilder {
+public:
+    StringBuilder() { }
+    ~StringBuilder() { }
+
+    void append(String&&);
+    void append(char);
+
+    String build();
+
+private:
+    Vector<String> m_strings;
+};
+
+}
+
+using AK::StringBuilder;
